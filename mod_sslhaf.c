2a28a4ddf1ac071f21738ca09c01f2d1f8279e1e
Daniel Berzin Chua <dbc@u.nus.edu>
2020-03-17 03:24:34 +0800

    Fix MD5 hash calls

--- mod_sslhaf.c
+++ mod_sslhaf.c
@@ -261,19 +261,24 @@ char *generate_sha1(apr_pool_t *pool, char *data, int len) {
     return bytes2hex(pool, digest, APR_SHA1_DIGESTSIZE);
 }
 
-char *generate_ja3(char *ver, char *ciph, char *ext, char *ec, char *ec_pf) {
-    unsigned char digest[APR_MD5_DIGESTSIZE];
+char *generate_ja3(apr_pool_t *pool, unsigned char * digest, char *ver, char *ciph, char *ext, char *ec, char *ec_pf) {
     apr_md5_ctx_t context;
 
     apr_md5_init(&context);
-    apr_md5_update(&context, (const char *)(strcat(ver, ",")), len);
-    apr_md5_update(&context, (const char *)(strcat(ciph, ",")) len);
-    apr_md5_update(&context, (const char *)(strcat(ext, ",")), len);
-    apr_md5_update(&context, (const char *)(strcat(ec, ",")), len);
-    apr_md5_update(&context, (const char *)ec_pf, len);
+    apr_md5_update(&context, (const char *)ver, strlen(ver));
+    apr_md5_update(&context, ",", 1);
+    apr_md5_update(&context, (const char *)ciph, strlen(ciph));
+    apr_md5_update(&context, ",", 1);
+    apr_md5_update(&context, (const char *)ext, strlen(ext));
+    apr_md5_update(&context, ",", 1);
+    apr_md5_update(&context, (const char *)ec, strlen(ec));
+    apr_md5_update(&context, ",", 1);
+    apr_md5_update(&context, (const char *)ec_pf, strlen(ec_pf));
     apr_md5_final(digest, &context);
-    return digest;
+    
+    return bytes2hex(pool, digest, APR_MD5_DIGESTSIZE);
 }
+
 /**
  * Convert one byte into its hexadecimal representation.
  */
@@ -1199,8 +1204,11 @@ static int sslhaf_post_request(request_rec *r) {
         if (cfg->request_counter == 1) {
             apr_table_setn(r->subprocess_env, "SSLHAF_LOG", "1");
         }
-
-        apr_table_setn(r->subprocess_env, "JA3_HASH", generate_ja3(protocol_ver, suites, ext, curves, ec_pt))
+        // char * hash = calloc(APR_MD5_DIGESTSIZE, 1);
+        // strcpy(hash, generate_ja3(protocol_ver, suites, ext, curves, ec_pt));
+        unsigned char digest[APR_MD5_DIGESTSIZE];
+        const char * ja3 = generate_ja3(r->connection->pool, digest, protocol_ver, suites, ext, curves, ec_pt);
+        apr_table_setn(r->subprocess_env, "JA3_HASH", ja3);
         
         #if 0
         // Generate a sha1 of the remote address on the first request
